2a94f6263a15f643999082fb1e2bbdec511808c2
Ahsan Ra2a <ahsan.m.amin210@gmail.com>
2024-04-17 00:46:53 +0500

    main game file

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "tic-tac-toe.h"
+#include <stdio.h>
+
+int main(void)
+{
+    char board[BOARD_SIZE][BOARD_SIZE] = {
+        {'.', '.', '.'},
+        {'.', '.', '.'},
+        {'.', '.', '.'}
+    };
+
+    int choice;
+
+    do 
+    {
+
+    } while (choice != 0);
+}
