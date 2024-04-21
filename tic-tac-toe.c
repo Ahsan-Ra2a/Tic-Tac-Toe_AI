7e1a73d2a15eb2782617c56376c6e3dc2e0a33ba
Ahsan Raza <167077860+Ahsan-Ra2a@users.noreply.github.com>
2024-04-21 12:45:56 +0500

    Update tic-tac-toe.c

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -158,14 +158,16 @@ void board_print(char board[BOARD_SIZE][BOARD_SIZE])
 	printf(" %c | %c | %c ",board[1][0],board[1][1],board[1][2]);
 	printf("\n---|---|---\n");
 	printf(" %c | %c | %c \n",board[2][0],board[2][1],board[2][2]);
- }
- void reset_board(char board[BOARD_SIZE][BOARD_SIZE]){
- 	for(i=0;i<3;i++)
+	printf("\n");
+}
+ void reset_board(char board[BOARD_SIZE][BOARD_SIZE])
+{
+	for(int i = 0; i < BOARD_SIZE; i++)
  	{
- 		for(j=0;j<3;j++)
-		 {
- 			board[i][j]='.';
-		 }
+		for(int j = 0; j < BOARD_SIZE; j++)
+		{
+ 			board[i][j]=EMPTY_CELL;
+		}
 	 }
  }
 
